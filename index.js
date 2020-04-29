7d48e110102bd0bb586a1573515a729f3cb06590
rodrigo <rodrigo19-1_1998@hotmail.com>
2020-04-28 23:53:18 -0300

    k

--- index.js
+++ index.js
@@ -8,31 +8,6 @@ app.use(express.static(path.join(__dirname, '/')));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-
-app.get('/lagaferia', function (req, res) {
-    res.sendFile(__dirname + "/lagaferia/index.html");
-});
-
-app.get('/soy', function (req, res) {
-  res.sendFile(__dirname + "/soy/index.html");
-});
-
-app.get('/lupenni', function (req, res) {
-    res.sendFile(__dirname + "/lupenni/index.html");
-});
-
-app.get('/lupenni-cel', function (req, res) {
-  res.sendFile(__dirname + "/lupenni-cel/index.html");
-});
-
-app.get('/victoria', function (req, res) {
-    res.sendFile(__dirname + "/victoria/index.html");
-});
-
-app.get('/Invitacion', function (req, res) {
-  res.sendFile(__dirname + "/Invitacion/index.html");
-});
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
