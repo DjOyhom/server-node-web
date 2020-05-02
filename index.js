0b8922e38b3a14c55270d8dbded5c5ec0b8659de
Rodrigo Hernández <rodrigo19-1_1998@hotmail.com>
2020-05-02 17:30:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.static(path.join(__dirname, '/')));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
 
   
